a391dff7f970aa3e37d877f2c8588b214b82611e
glitchbhi <glitchbhi@gmail.com>
2026-02-05 19:00:40 +0600

    Add favicon and ensure Thunder Booking shows in browser tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Thunder Booking') }} - @yield('title', 'Sports Ground Booking')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">    <link rel=\"icon\" type=\"image/x-icon\" href=\"{{ asset('favicon.ico') }}\">    <title>{{ config('app.name', 'Thunder Booking') }} - @yield('title', 'Sports Ground Booking')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
